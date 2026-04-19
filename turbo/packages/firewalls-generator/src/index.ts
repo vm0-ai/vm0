f63b8749e131dfad8cedeef03044bb26880ff024
Ethan Zhang <perfectworks@gmail.com>
2026-04-19 17:15:20 +0800

    feat: add groq to firewall generator index

--- turbo/packages/firewalls-generator/src/index.ts
+++ turbo/packages/firewalls-generator/src/index.ts
@@ -61,6 +61,7 @@ import { generate as generateGitHub } from "./github";
 import { generate as generateGitlab } from "./gitlab";
 import { generate as generateGranola } from "./granola";
 import { generate as generateGreenhouse } from "./greenhouse";
+import { generate as generateGroq } from "./groq";
 import { generate as generateHeygen } from "./heygen";
 import { generate as generateHtmlcsstoimage } from "./htmlcsstoimage";
 import { generate as generateHubspot } from "./hubspot";
@@ -210,6 +211,7 @@ const GENERATORS: Record<string, () => Promise<void>> = {
   gitlab: generateGitlab,
   granola: generateGranola,
   greenhouse: generateGreenhouse,
+  groq: generateGroq,
   heygen: generateHeygen,
   htmlcsstoimage: generateHtmlcsstoimage,
   hubspot: generateHubspot,
