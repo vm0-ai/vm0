3f709350035435aede30d0d6230df16f5203c5c9
Liang You <liangyou@vm0.ai>
2026-05-31 18:44:12 +0800

    chore: regenerate connector auth method migration

--- turbo/packages/db/src/migrations/0415_dashing_sugar_man.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "connector_oauth_device_authorization_sessions" ADD COLUMN "auth_method" varchar(50);--> statement-breakpoint
-UPDATE "connector_oauth_device_authorization_sessions" SET "auth_method" = 'oauth' WHERE "auth_method" IS NULL;--> statement-breakpoint
-ALTER TABLE "connector_oauth_device_authorization_sessions" ALTER COLUMN "auth_method" SET NOT NULL;
