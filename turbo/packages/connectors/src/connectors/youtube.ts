cf1889789a9952e8d1ab9c2dc989c11ae2af8c64
Linghan Hu <yuma@vm0.ai>
2026-06-17 10:58:08 +0000

    fix: remove deprecated youtube oauth scopes

--- turbo/packages/connectors/src/connectors/youtube.ts
+++ turbo/packages/connectors/src/connectors/youtube.ts
@@ -27,9 +27,6 @@ export const youtube = {
             "https://www.googleapis.com/auth/youtube.force-ssl",
             "https://www.googleapis.com/auth/youtube.readonly",
             "https://www.googleapis.com/auth/youtube.upload",
-            "https://www.googleapis.com/auth/youtube.channel-memberships.creator",
-            "https://www.googleapis.com/auth/youtubepartner",
-            "https://www.googleapis.com/auth/youtubepartner-channel-audit",
             "https://www.googleapis.com/auth/userinfo.email",
           ],
           outputs: {
