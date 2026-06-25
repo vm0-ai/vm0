a0057dc259a0021eb8445f687efb5338969b784f
seven332 <liangyou@vm0.ai>
2026-06-25 14:57:44 +0800

    test(vsock-guest): assert guest connection thread results (#18913)

--- crates/vsock-guest/tests/connection/support/connection.rs
+++ crates/vsock-guest/tests/connection/support/connection.rs
@@ -1,3 +1,4 @@
+use std::os::unix::net::UnixStream;
 use std::thread;
 use std::time::Duration;
 
@@ -5,11 +6,11 @@ use vsock_guest::handle_connection;
 
 use super::protocol::read_and_discard_message;
 
-pub(crate) fn start_guest_connection() -> (thread::JoinHandle<()>, std::os::unix::net::UnixStream) {
-    let (guest_stream, mut host_stream) = std::os::unix::net::UnixStream::pair().unwrap();
-    let handle = thread::spawn(move || {
-        let _ = handle_connection(guest_stream);
-    });
+pub(crate) type GuestConnectionHandle = thread::JoinHandle<std::io::Result<()>>;
+
+pub(crate) fn start_guest_connection() -> (GuestConnectionHandle, UnixStream) {
+    let (guest_stream, mut host_stream) = UnixStream::pair().unwrap();
+    let handle = thread::spawn(move || handle_connection(guest_stream));
     read_and_discard_message(&mut host_stream);
     host_stream
         .set_read_timeout(Some(Duration::from_secs(5)))
@@ -17,10 +18,12 @@ pub(crate) fn start_guest_connection() -> (thread::JoinHandle<()>, std::os::unix
     (handle, host_stream)
 }
 
-pub(crate) fn finish_guest_connection(
-    handle: thread::JoinHandle<()>,
-    host_stream: std::os::unix::net::UnixStream,
-) {
+pub(crate) fn finish_guest_connection(handle: GuestConnectionHandle, host_stream: UnixStream) {
     drop(host_stream);
-    let _ = handle.join();
+    join_guest_connection(handle);
+}
+
+pub(crate) fn join_guest_connection(handle: GuestConnectionHandle) {
+    let result = handle.join().expect("guest connection thread panicked");
+    result.expect("guest connection handler returned an error");
 }
